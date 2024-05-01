a17eb9c5239b8f0ac46b20d24012ce9be9c482e7
OCCCAS <pavlov.timur556@yandex.ru>
2024-05-01 10:53:27 +0300

    fix: confirmation code input type set to tel

--- online_signature/forms.py
+++ online_signature/forms.py
@@ -3,12 +3,9 @@ from django.conf import settings
 
 
 class SignDocumentForm(forms.Form):
-    class Media:
-        js = ["js/confirmation_code_again_counter.js"]
-
     confirmation_code = forms.CharField(
         max_length=settings.CONFIRMATION_CODE_LENGTH,
         widget=forms.TextInput(
-            attrs={"placeholder": "Код из СМС", "class": "form__input"}
+            attrs={"placeholder": "Код из СМС", "class": "form__input", "type": "tel"}
         ),
     )
