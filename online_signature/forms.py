2375186af29c1d2ea185e1fe7c78d15ebcc24892
OCCCAS <pavlov.timur556@yandex.ru>
2024-05-01 11:28:39 +0300

    fix: added only numbers filter to confirmation code input and keyboard type tel

--- online_signature/forms.py
+++ online_signature/forms.py
@@ -4,8 +4,13 @@ from django.conf import settings
 
 class SignDocumentForm(forms.Form):
     confirmation_code = forms.CharField(
+        label="",
         max_length=settings.CONFIRMATION_CODE_LENGTH,
         widget=forms.TextInput(
-            attrs={"placeholder": "Код из СМС", "class": "form__input", "type": "tel"}
+            attrs={
+                "placeholder": "Код из СМС",
+                "class": "form__input",
+                "type": "tel",
+            }
         ),
     )
